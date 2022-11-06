0792867451fa774dfeb327aa85cd8b9af44d1293
MatDev435 <matdev435@gmail.com>
2022-11-05 23:13:26 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import Fastify from 'fastify';
+import corst from '@fastify/cors';
+import jwt from '@fastify/jwt';
+
+import { authRoutes } from './routes/auth';
+import { gameRoutes } from './routes/game';
+import { guessRoutes } from './routes/guess';
+import { poolRoutes } from './routes/pool';
+import { userRoutes } from './routes/user';
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    await fastify.register(corst, {
+        origin: true,
+    })
+
+    fastify.register(jwt, {
+        secret: 'maisvapraporrahomiasçdlckmasdlçkcjasdklfjKLÇAMSDLCKMkjasdkcasdcasdceasdcKJkmkJKHASDCMKmkajsdcçmkleeesdc',
+    });
+
+    await fastify.register(authRoutes);
+    await fastify.register(gameRoutes);
+    await fastify.register(guessRoutes);
+    await fastify.register(poolRoutes);
+    await fastify.register(userRoutes);
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
+}
+
+bootstrap();
